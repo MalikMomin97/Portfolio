deac72d85013c5621c00e0c23b6ec74cca6310b4
Salmaan Mushtaq <salman@imobisoft.co.uk>
2025-08-11 21:43:27 +0530

    fix: build error fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
+import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-import { resolve } from 'node:path'
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [viteReact(), tailwindcss()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
